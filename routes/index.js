da2a8dd7d2eea6300ed95bdb1cc4ac588e6c2472
NHatim <hatim.naimi@gmail.com>
2022-05-11 12:07:08 +0200

    clients + areainfos

--- routes/index.js
+++ routes/index.js
@@ -2,14 +2,31 @@ var express = require('express');
 var router = express.Router();
 require('dotenv').config();
 const axios = require('axios')
+const headers = {
+  hash:'',
+  time: '',
+  teamUUID : process.env.teamUUID
+}
+const baseURL = 'http://192.168.0.190:8080'
 
-const token = () => {
+const token = async () => {
  
-    const res =  axios.get(`http://192.168.0.190:8080/api/security/token/${process.env.teamUUID}`).then(res => console.log(res));
+    const res =  await axios.get(`${baseURL}/api/security/token/${process.env.teamUUID}`);
+    headers.hash = res.data.hash;
+    headers.time = res.data.time;
+    console.log(headers)
 
-} 
+}
 
-setInterval(token,1000)
+const getAreasClients = async () => {
+  await token()
+  const area = await axios.get(`${baseURL}/api/game/areainfos`, {headers: headers})
+  const clients = await axios.get(`${baseURL}/api/game/clients`, {headers: headers});
+  console.log(area.data, clients.data);
+
+}
+getAreasClients()
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
